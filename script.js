68cf47605f2c6a3e1ee0fa86d1080439192b2dc8
Ashutosh <omkarashu28@gmail.com>
2022-12-12 01:20:48 +0530

    Added answer block on creation of flash card

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const addFlashCard = document.querySelector(".addFlashCard");
-const addQuestion = document.getElementById("");
-const addAnswer = document.getElementById("");
-const updateCard = document.getElementById("");
-const deleteCard = document.getElementById("");
+const addQuestion = document.querySelector("");
+const addAnswer = document.querySelector("");
+const updateCard = document.querySelector("");
+const deleteCard = document.querySelector("");
+const closeBtn = document.querySelector("closeBtn");
 const flashCardQuestion = document.querySelector(".flashCardQuestion");
 
-addFlashCard.addEventListener("click", () => {
-  flashCardQuestion.style.display = "block";
-});
+// addFlashCard.addEventListener("click", () => {
+//   flashCardQuestion.style.display = "block";
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   flashCardQuestion.style.display = "none";
+// });
