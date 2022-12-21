153a20757feee9025cb6714f7567bb25931e7a60
Ashutosh <omkarashu28@gmail.com>
2022-12-22 00:49:33 +0530

    SOme minor  bugs fix in card

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const closeBtn = document.querySelector("#closeBtn");
 const createCard = document.querySelector("#createBtn");
 
 const flashBlock = document.querySelector(".flashBlock");
+const answerValue = document.querySelector(".answerValue");
 
 addFlashCard.addEventListener("click", () => {
   flashCardQuestion.style.display = "block";
+  flashBlock.style.display = "none";
 });
 
 closeBtn.addEventListener("click", () => {
@@ -18,3 +20,7 @@ createCard.addEventListener("click", () => {
   flashBlock.style.display = "flex";
   flashCardQuestion.style.display = "none";
 });
+
+function hideValue() {
+  answerValue.style.display = "none";
+}
